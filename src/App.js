1f75f1c7cdf111ccf7ff75f843004eb49c9a0df7
Yibran A. Calidonio <yibrancalidonio@gmail.com>
2024-03-11 12:48:37 -0400

    Connected to backend

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import Form from "./Form"
 import React, { useState, useEffect } from 'react';
 
 function App() {
-    return (
-      <div className="App">
-        <Form />
-      </div>
-    );
+  const [message, setMessage] = useState('');
+  const homeEndpoint = process.env.REACT_APP_HOME_ENDPOINT;
+  useEffect(() => {
+    async function fetchHomePage() {
+      try {
+        const response = await fetch(homeEndpoint);
+        const data = await response.json();
+        setMessage(data.Message); // Extracting the message from the response
+      } catch (error) {
+        console.error('Error fetching homepage:', error);
+      }
+    }
+
+    fetchHomePage();
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Home Page</h1>
+      <p>{message}</p>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
