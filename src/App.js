afb19dc325b2677b7d8173e3faa2391231f3813a
Yibran A. Calidonio <yibrancalidonio@gmail.com>
2024-03-11 12:40:57 -0400

    connected Frontend wit backend and added .env for urls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Form from "./Form"
+import React, { useState, useEffect } from 'react';
 
 function App() {
-return (
-<div className="App">
-<Form />
-</div>
-
-);
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
 }
 
 export default App;
