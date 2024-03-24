f035e94b29e01a17781a5d6517e91c468210e3dd
jgb36 <jgb36@njit.edu>
2024-03-24 14:08:51 -0400

    finalized login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-import RegistrationForm from "./Registration"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginForm from './pages/Login';
+import RegistrationForm from './pages/Registration';
 
 function App() {
-return (
-<div className="App">
-<RegistrationForm />
-</div>
-
-);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegistrationForm />} />
+        {/* Redirect from base URL to /register */}
+        <Route path="/" element={<Navigate replace to="/register" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
